204b66fa2936784bd9340f2a1b26ef09df8c5576
Rosnye <lamaso02david@gmail.com>
2023-10-24 13:32:22 -0600

    feat this proyect description added

--- src/components/Proyectos/index.jsx
+++ src/components/Proyectos/index.jsx
@@ -1,9 +1,24 @@
-import { Container } from "@mui/material"
+import { Box, Container } from "@mui/material"
 
 const Proyectos = () => {
     return(
         <Container>
-            
+            <Box style={{display:"flex",justifyContent:"space-evenly"}}>
+                <img src="src\assets\react.svg" alt="react svg" style={{height:"10rem"}}/>
+                <h2 style={{display:"flex", justifyContent:"center", width:"50px", fontSize:"40px"}}>+</h2>
+                <img src="https://www.svgrepo.com/show/374167/vite.svg" alt="vite svg" style={{height:"10rem"}} />
+            </Box>
+            <Box>
+                <h2>Cutting-Edge Project Technologies: React + Vite</h2>
+                <p>
+                This project was developed using cutting-edge technologies. 
+                React and Vite were employed as the primary framework for web application development, 
+                harnessing efficiency and speed to the fullest. Additionally, the popular Material UI 
+                library was integrated to achieve an appealing and functional user interface. 
+                Finally, the project is hosted on GitHub Pages for public access and viewing, 
+                ensuring a smooth and accessible online user experience.
+                </p>
+            </Box>
         </Container>
     )
 }
